e6d171969c7631f757efc1de926558e0ff6b1704
VadimPopov <vessims@gmail.com>
2023-02-26 13:18:41 +0300

    To Do list added

---
+++ VoiceRecorder/TODO List.swift
@@ -0,0 +1,31 @@
+//
+//  TODO List.swift
+//  VoiceRecorder
+//
+//  Created by Vadim Popov on 26.02.2023.
+//
+
+
+// MARK: - Home VC
+
+
+// MARK: - Search VC
+
+// TODO: this
+
+
+// MARK: - Record VC
+
+// TODO: this
+
+
+// MARK: - History VC
+
+// TODO: slider on playing audio
+// TODO: red button if isSelected
+
+
+// MARK: - Settings VC
+
+// TODO: this
+
